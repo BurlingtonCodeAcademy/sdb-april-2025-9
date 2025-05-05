ac6d8b56eb8484cac8498107dab25ba8f6a0f744
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2025-05-05 17:41:22 -0500

    for example

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 // while example
-let x = 1;
+let x = 3;
 
-while (x < 11) {
-    console.log("HAPPY MONDAY!", x)
-    x = x + 1
+while (x <= 9) {
+    console.log("While count: ", x);
+    x++;
+}
+
+// for example
+for (let i = 3; i <= 9; i++) {
+    // the code to be executed
+    console.log("For count: ", i);
 }
